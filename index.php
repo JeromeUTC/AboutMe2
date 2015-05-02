c430dbeedce12f303ece8c552ef3a8c30f2ff986
JeromeUTC <jerome.thiebaut@etu.utc.fr>
2015-05-02 19:14:12 +0200

    V1
    test

---
+++ index.php
@@ -0,0 +1,85 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+	<meta http-equiv="Content-Type" Content="text/html; charset=UTF-8">
+	<title>Antoine Barbare</title>
+	<link rel="stylesheet" href="style.css">
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,300italic,600italic,400,600,700,800' rel='stylesheet' type='text/css'>
+    <meta http-equiv="X-UA-Compatible" content="IE=8" />
+    <!--[if lt IE 9]>
+	    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+	</head>
+<body>
+
+	<header>
+		<a id="logo" href='index.php'>Antoine Barbare</a>
+		<nav>
+			<li><a href='#about'>À Propos</a></li>
+			<li><a href='#skills'>Compétences</a></li>
+			<li><a href='#resume'>CV</a></li>
+			<li><a href='#contact'>Contact</a></li>
+		</nav>
+		<div id="menu_responsive">
+		
+		<select onchange="window.location.href=this.options[this.selectedIndex].value">			
+			<option selected="true" disabled="" >Menu</option>
+			<option value="#about">À Propos</option>
+			<option value="#skills">Compétences</option>
+			<option value="#resume">CV</option>
+			<option value="#contact">Contact</option>
+		</select>		
+		</div>
+		
+		<ul class="social">
+			<li class="facebook"><a href='https://facebook.com/HelpComputers' title="Like me on Facebook" target="_blank">facebook</a></li>
+			<li class="twitter"><a href='https://twitter.com/antoine_geek' title="Follow me on Twitter" target="_blank">twitter</a></li>
+			<li class="dribbble"><a href='http://fr.viadeo.com/fr/profile/antoine.barbare' title="Viadeo" target="_blank">dribbble</a></li>
+			<li class="linkedin"><a href='http://www.linkedin.com/pub/antoine-barbare/49/a/8b5' title="Linkedin" target="_blank">linkedin</a></li>
+		</ul>
+	</header>
+	
+	<div id="container">
+		<section id="about">
+			<div class="text_left">
+			<span class="macouleur1">About </span><span class="macouleur2">Me</span>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam est dolor, interdum at elementum eget, mollis eget tortor. Vestibulum tempus imperdiet metus non tempor. Ut vitae leo sollicitudin dolor cursus porta. Quisque ac aliquet ligula, eu sodales sem. Maecenas accumsan iaculis sagittis. Duis tempus volutpat mi, quis luctus urna luctus id. Vestibulum interdum accumsan tellus, et volutpat tellus tristique non. nostra, per inceptos himenaeos. Phasellus et vehicula sem, iaculis blandit elit. Donec sagittis luctus tortor eget laoreet. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
+			</div>
+				<img src="img/carre.png" alt="me" class="photo"/>
+		</section>
+		
+		<section id="skills">
+		<span class="macouleur1">Mes </span><span class="macouleur2">Compétences</span>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam est dolor, interdum at elementum eget, mollis eget tortor. Vestibulum tempus imperdiet metus non tempor. Ut vitae leo sollicitudin dolor cursus porta. Quisque ac aliquet ligula, eu sodales sem. Maecenas accumsan iaculis sagittis. Duis tempus volutpat mi, quis luctus urna luctus id. Vestibulum interdum accumsan tellus, et volutpat tellus tristique non.
+
+Sed consectetur sit amet sem nec lobortis. Aliquam nec vestibulum erat. Praesent arcu neque, auctor non sapien rutrum, volutpat imperdiet velit. Nam dignissim, elit sed sodales accumsan, sem sapien sagittis est, eu euismod leo leo vel sem. Vestibulum eu tincidunt elit, sit amet aliquam purus. Donec libero lorem, consequat vel tellus et, consectetur congue ligula. Vivamus id elit eu massa interdum tempor id eu diam. Curabitur mauris nisi, consequat eget massa at, ultricies laoreet nulla. Pellentesque vel scelerisque neque. Nam massa nunc, cursus in pharetra non, venenatis vitae neque. Donec gravida risus a enim lobortis tristique. Duis tempor sem nunc, et blandit risus ullamcorper vel. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus et vehicula sem, iaculis blandit elit. Donec sagittis luctus tortor eget laoreet. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam est dolor, interdum at elementum eget, mollis eget tortor. Vestibulum tempus imperdiet metus non tempor. Ut vitae leo sollicitudin dolor cursus porta. Quisque ac aliquet ligula, eu sodales sem. Maecenas accumsan iaculis sagittis. Duis tempus volutpat mi, quis luctus urna luctus id. Vestibulum interdum accumsan tellus, et volutpat tellus tristique non.
+
+Sed consectetur sit amet sem nec lobortis. Aliquam nec vestibulum erat. Praesent arcu neque, auctor non sapien rutrum, volutpat imperdiet velit. Nam dignissim, elit sed sodales accumsan, sem sapien sagittis est, eu euismod leo leo vel sem. Vestibulum eu tincidunt elit, sit amet aliquam purus. Donec libero lorem, consequat vel tellus et, consectetur congue ligula. Vivamus id elit eu massa interdum tempor id eu diam. Curabitur mauris nisi, consequat eget massa at, ultricies laoreet nulla. Pellentesque vel scelerisque neque. Nam massa nunc, cursus in pharetra non, venenatis vitae neque. Donec gravida risus a enim lobortis tristique. Duis tempor sem nunc, et blandit risus ullamcorper vel. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus et vehicula sem, iaculis blandit elit. Donec sagittis luctus tortor eget laoreet. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
+		</section>
+		<section id="resume">
+		<span class="macouleur1">Mon </span><span class="macouleur2">Curriculum Vitae</span>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam est dolor, interdum at elementum eget, mollis eget tortor. Vestibulum tempus imperdiet metus non tempor. Ut vitae leo sollicitudin dolor cursus porta. Quisque ac aliquet ligula, eu sodales sem. Maecenas accumsan iaculis sagittis. Duis tempus volutpat mi, quis luctus urna luctus id. Vestibulum interdum accumsan tellus, et volutpat tellus tristique non.
+
+Sed consectetur sit amet sem nec lobortis. Aliquam nec vestibulum erat. Praesent arcu neque, auctor non sapien rutrum, volutpat imperdiet velit. Nam dignissim, elit sed sodales accumsan, sem sapien sagittis est, eu euismod leo leo vel sem. Vestibulum eu tincidunt elit, sit amet aliquam purus. Donec libero lorem, consequat vel tellus et, consectetur congue ligula. Vivamus id elit eu massa interdum tempor id eu diam. Curabitur mauris nisi, consequat eget massa at, ultricies laoreet nulla. Pellentesque vel scelerisque neque. Nam massa nunc, cursus in pharetra non, venenatis vitae neque. Donec gravida risus a enim lobortis tristique. Duis tempor sem nunc, et blandit risus ullamcorper vel. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus et vehicula sem, iaculis blandit elit. Donec sagittis luctus tortor eget laoreet. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam est dolor, interdum at elementum eget, mollis eget tortor. Vestibulum tempus imperdiet metus non tempor. Ut vitae leo sollicitudin dolor cursus porta. Quisque ac aliquet ligula, eu sodales sem. Maecenas accumsan iaculis sagittis. Duis tempus volutpat mi, quis luctus urna luctus id. Vestibulum interdum accumsan tellus, et volutpat tellus tristique non.
+
+Sed consectetur sit amet sem nec lobortis. Aliquam nec vestibulum erat. Praesent arcu neque, auctor non sapien rutrum, volutpat imperdiet velit. Nam dignissim, elit sed sodales accumsan, sem sapien sagittis est, eu euismod leo leo vel sem. Vestibulum eu tincidunt elit, sit amet aliquam purus. Donec libero lorem, consequat vel tellus et, consectetur congue ligula. Vivamus id elit eu massa interdum tempor id eu diam. Curabitur mauris nisi, consequat eget massa at, ultricies laoreet nulla. Pellentesque vel scelerisque neque. Nam massa nunc, cursus in pharetra non, venenatis vitae neque. Donec gravida risus a enim lobortis tristique. Duis tempor sem nunc, et blandit risus ullamcorper vel. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus et vehicula sem, iaculis blandit elit. Donec sagittis luctus tortor eget laoreet. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
+		</section>
+		<section id="contact">
+		<span class="macouleur1">Me </span><span class="macouleur2">Contacter</span>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam est dolor, interdum at elementum eget, mollis eget tortor. Vestibulum tempus imperdiet metus non tempor. Ut vitae leo sollicitudin dolor cursus porta. Quisque ac aliquet ligula, eu sodales sem. Maecenas accumsan iaculis sagittis. Duis tempus volutpat mi, quis luctus urna luctus id. Vestibulum interdum accumsan tellus, et volutpat tellus tristique non.
+
+Sed consectetur sit amet sem nec lobortis. Aliquam nec vestibulum erat. Praesent arcu neque, auctor non sapien rutrum, volutpat imperdiet velit. Nam dignissim, elit sed sodales accumsan, sem sapien sagittis est, eu euismod leo leo vel sem. Vestibulum eu tincidunt elit, sit amet aliquam purus. Donec libero lorem, consequat vel tellus et, consectetur congue ligula. Vivamus id elit eu massa interdum tempor id eu diam. Curabitur mauris nisi, consequat eget massa at, ultricies laoreet nulla. Pellentesque vel scelerisque neque. Nam massa nunc, cursus in pharetra non, venenatis vitae neque. Donec gravida risus a enim lobortis tristique. Duis tempor sem nunc, et blandit risus ullamcorper vel. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus et vehicula sem, iaculis blandit elit. Donec sagittis luctus tortor eget laoreet. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam est dolor, interdum at elementum eget, mollis eget tortor. Vestibulum tempus imperdiet metus non tempor. Ut vitae leo sollicitudin dolor cursus porta. Quisque ac aliquet ligula, eu sodales sem. Maecenas accumsan iaculis sagittis. Duis tempus volutpat mi, quis luctus urna luctus id. Vestibulum interdum accumsan tellus, et volutpat tellus tristique non.
+
+Sed consectetur sit amet sem nec lobortis. Aliquam nec vestibulum erat. Praesent arcu neque, auctor non sapien rutrum, volutpat imperdiet velit. Nam dignissim, elit sed sodales accumsan, sem sapien sagittis est, eu euismod leo leo vel sem. Vestibulum eu tincidunt elit, sit amet aliquam purus. Donec libero lorem, consequat vel tellus et, consectetur congue ligula. Vivamus id elit eu massa interdum tempor id eu diam. Curabitur mauris nisi, consequat eget massa at, ultricies laoreet nulla. Pellentesque vel scelerisque neque. Nam massa nunc, cursus in pharetra non, venenatis vitae neque. Donec gravida risus a enim lobortis tristique. Duis tempor sem nunc, et blandit risus ullamcorper vel. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus et vehicula sem, iaculis blandit elit. Donec sagittis luctus tortor eget laoreet. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
+		</section>
+		<section id="footer">
+			<footer>
+			<small>Antoine Barbare &copy;&nbsp;<?php echo date('Y');?></small>
+			<small><a href="mention.php" title="Mentions">Mentions Légales</a></small>
+			</footer>
+		</section>
+	</div>
+
+</body>
+</html>
